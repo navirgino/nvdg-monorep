19c51ab1d2d8cf035d1742f4a3d6c044065f5e42
Nico Virgino <nicovirgino@gmail.com>
2023-01-02 22:28:59 -0600

    boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CardComponent } from './shared/card/card.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent,
+    InventoryComponent,
+    BlogComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
