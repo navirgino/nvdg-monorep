0596dacf9c6a7fceb47cb155d9ec8408daa48b23
Nico Virgino <nicovirgino@gmail.com>
2023-12-14 21:52:27 -0600

    catching up

---
+++ projects/portfolio/src/lib/portfolio.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PortfolioComponent } from './portfolio.component';
+
+
+
+@NgModule({
+  declarations: [
+    PortfolioComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    PortfolioComponent
+  ]
+})
+export class PortfolioModule { }
